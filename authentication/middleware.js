3047d08beaa0cea84db027955372dc873d244bc1
dukestabhi <new.abhishekh@gmail.com>
2020-01-20 08:57:32 +0545

    Initial Backend

---
+++ authentication/middleware.js
@@ -0,0 +1,12 @@
+//to way communication for req and res 
+
+const auth = function(req, res, next)
+{
+    console.log("this is middleware");
+
+}
+module.exports =auth; //neds authentication
+
+//copy paste above code for middleware
+
+//implementing a system for users in.
